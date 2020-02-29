3bcf9e3c5a9e25432b009396219dcbb0fa00f9f8
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-29 11:45:23 -0300

    mess with lint

--- src/atlas/controllers/operation.clj
+++ src/atlas/controllers/operation.clj
@@ -2,5 +2,6 @@
   (:require [common-clj.http-client.protocol :as hc-pro]))
 
 (defn get-operations [service {:keys [http-client]}]
+
   (let [path-params {:path-params {:service service}}]
     (:data (:body (hc-pro/request http-client :jaeger/get-operations path-params)))))
