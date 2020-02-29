8eff047d8590e9317324ed0ce9f7b265dc863570
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-29 11:52:20 -0300

    fix

--- src/atlas/controllers/operation.clj
+++ src/atlas/controllers/operation.clj
@@ -2,7 +2,5 @@
   (:require [common-clj.http-client.protocol :as hc-pro]))
 
 (defn get-operations [service {:keys [http-client]}]
-
   (let [path-params {:path-params {:service service}}]
-
     (:data (:body (hc-pro/request http-client :jaeger/get-operations path-params)))))
