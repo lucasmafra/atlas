f9e4724b893b014b39ecefbadc5aafb15c5fe459
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-29 00:22:22 -0300

    fix test but mess with lint

--- src/atlas/controllers/operation.clj
+++ src/atlas/controllers/operation.clj
@@ -2,5 +2,7 @@
   (:require [common-clj.http-client.protocol :as hc-pro]))
 
 (defn get-operations [service {:keys [http-client]}]
-  (let [path-params {:path-params {:service service}}]
+  (let [path-params {:path-params {:service service}}
+
+        a 1]
     (:data (:body (hc-pro/request http-client :jaeger/get-operations path-params)))))
