628345d7fb5fc38973ce1bd01fb4f53d4f544072
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-01 00:36:54 -0300

    bump common-clj to leverage new test helpers

--- test/flows/get_services.clj
+++ test/flows/get_services.clj
@@ -1,18 +1,16 @@
 (ns flows.get-services
-  (:require [common-clj.state-flow-helpers.config :as config]
-            [common-clj.state-flow-helpers.http-client :as http-client]
-            [common-clj.state-flow-helpers.http-server :as http-server]
+  (:require [common-clj.state-flow-helpers.http-client :as http-client]
+            [common-clj.state-flow-helpers.http-server :refer [GET]]
             [flows.aux.init :refer [defflow]]
             [state-flow.assertions.matcher-combinators :refer [match?]]))
 
-(def mock-http-client
-  {"http://jaeger.com/api/services" {:status 200 :body {:data ["orders" "feed"]}}})
+(def mock-http-calls
+  {"{{jaeger}}/api/services" {:status 200 :body {:data ["orders" "feed"]}}})
 
 (defflow get-services
-  :pre-conditions [(config/assoc-in! [:known-hosts :jaeger] "http://jaeger.com")
-                   (http-client/mock! mock-http-client)]
+  :pre-conditions [(http-client/mock! mock-http-calls)]
 
-  [response (http-server/request-arrived! :get "/api/services")]
+  [response (GET "/api/services")]
 
   (match? {:status 200 :body {"services" ["orders" "feed"]}}
           response))
